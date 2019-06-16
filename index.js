ea0700d2741854e9e1957646646cd7b61f57ab05
Ivickayl <Quentin.Scheidt@gmail.com>
2019-06-16 04:38:25 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+//const Discord = require("discord.js");
+const { Client, Attachment } = require("discord.js");
+const Config = require("./config.json");
+
+const prefix = process.env.PREFIX;
+const bot = new Client();
+
+function sendMessage(author, channel, content) {
+  if (typeof content === "string") {
+    channel.send(`${author} : ${content}`);
+  } else {
+    channel.send(`${author}`, content);
+  }
+}
+
+bot.on("ready", () => {
+  console.log("READY");
+});
+
+bot.on("message", message => {
+  if (
+    Config.nicknames.indexOf(message.author.username.toLocaleLowerCase()) !== -1
+  ) {
+    content = message.content;
+
+    const channel = message.guild.channels.find(
+      ch => ch.name === Config.channel
+    );
+
+    if (!channel) return;
+
+    sendMessage(message.author.username, channel, content);
+
+    for (const iterator of message.attachments) {
+      const attachment = new Attachment(iterator[1].url);
+      sendMessage(message.author.username, channel, attachment);
+    }
+  }
+});
+
+bot.login(process.env.BOT_TOKEN);
